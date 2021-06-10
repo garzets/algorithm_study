07ee9283ff753a59d6a994cbd5849941c082eab8
kimsanghyun <garzets@gmail.com>
2021-06-10 22:46:01 +0900

    p1707 solution
    must check unconnected graph, and just one vertice and edge.

--- baekjoon/p1707/sol.py
+++ baekjoon/p1707/sol.py
@@ -20,7 +20,6 @@ def DFS(node):
 
     return True
 
-
 def BFS(node):
     # graph :  [[2], [1, 3, 4], [2, 4], [3, 2], []]
     tmp_answer = True
@@ -37,7 +36,7 @@ def BFS(node):
                 #print("Not binary")
                 return False
 
-    print("\nqueue", node_queue)
+    #print("\nqueue", node_queue)
 
     while node_queue:
         if(BFS(node_queue.pop()) == False):
@@ -53,13 +52,18 @@ num_of_test = sys.stdin.readline()
 for test in range(1, int(num_of_test) + 1):
     V,E = sys.stdin.readline().split()
 
+    if V==1 and E==1:
+        v1, v2 = sys.stdin.readline().split()
+        print("YES")
+        continue
+
     graph = list()
     node_queue = list()
     #colored = list()
     color = list()
     answer = True
 
-    for i in range(1 + int(V)):
+    for i in range(int(V)):
         graph.append(list())
         #colored.append(0)
         color.append(0)
@@ -79,8 +83,43 @@ for test in range(1, int(num_of_test) + 1):
     #colored[0] = 1
     color[0] = 1
 
-    if BFS(0):
-        print("YES")
-    else:
-        print("NO")
+    # if BFS(0):
+    #     print("YES")
+    # else:
+    #     print("NO")
+
+    que = list()
+    que.append(0)
+    # print("push 0")
+    color[0]=1
+
+    answer = "YES"
+
+    while que:
+        node = que.pop()
+        # print("queue pop node",node)
+
+        for n in range(graph[node].__len__()):
+            if color[graph[node][n]-1] == 0 : # if next node was not yet visited
+                color[graph[node][n]-1] = color[node]*(-1) # set color different from current node
+                que.append(graph[node][n]-1)
+                # print("push ",graph[node][n]-1)
+            else : # if it visited, compare color
+                if color[graph[node][n]-1] == color[node] :
+                    answer = "NO"
+    
+        if not que:
+            try:
+                ret = color.index(0)
+                que.append(ret)
+                color[ret]=1
+                #print("uncolored index", ret)
+                #sys.exit(0)
+            except:
+                # print("there no uncolored")
+                break
+
+    # print(graph)
+    # print(color)
+    print(answer)
 

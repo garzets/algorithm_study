224e850e61923e463159d947791328fae2c26e06
kimsanghyun <garzets@gmail.com>
2021-06-10 19:53:27 +0900

    p1707 BFS, DFA cause runtime error

--- baekjoon/p1707/sol.py
+++ baekjoon/p1707/sol.py
@@ -1,14 +1,4 @@
-"""
-2       num of test case
-3 2     V,E value of test case 1
-1 3
-2 3
-4 4     V,E value of test case 2
-1 2
-2 3
-3 4
-4 2
-"""
+import sys
 
 def DFS(node):
     # graph :  [[2], [1, 3, 4], [2, 4], [3, 2], []]
@@ -30,14 +20,39 @@ def DFS(node):
     return True
 
 
-num_of_test = input()
+def BFS(node):
+    # graph :  [[2], [1, 3, 4], [2, 4], [3, 2], []]
+    tmp_answer = True
+    node_queue = list()
+
+    for n in range(graph[node].__len__()):
+        if(color[graph[node][n]-1]==0):
+            color[graph[node][n]-1]=color[node]*(-1)
+            node_queue.append(graph[node][n]-1)
+
+        else:
+            #print("compare coloe ",graph[node][n],color[graph[node][n]-1], color[node], node+1)
+            if color[graph[node][n]-1] == color[node]:
+                #print("Not binary")
+                return False
+
+    print("\nqueue", node_queue)
+
+    while node_queue:
+        if(BFS(node_queue.pop()) == False):
+            return False
+    else:
+        return True
+
+num_of_test = sys.stdin.readline()
 #print("number of test case ",num_of_test)
 
 
 for test in range(1, int(num_of_test) + 1):
-    V,E = input().split()
+    V,E = sys.stdin.readline().split()
 
     graph = list()
+    node_queue = list()
     #colored = list()
     color = list()
     answer = True
@@ -49,8 +64,7 @@ for test in range(1, int(num_of_test) + 1):
 
     #print("V, E ", V, E)
     for e in range(1, int(E) + 1):
-        #print(input())
-        v1, v2 = input().split()
+        v1, v2 = sys.stdin.readline().split()
         
         v1 = int(v1)
         v2 = int(v2)
@@ -63,25 +77,8 @@ for test in range(1, int(num_of_test) + 1):
     #colored[0] = 1
     color[0] = 1
 
-    if DFS(0):
+    if BFS(0):
         print("YES")
     else:
         print("NO")
-
-#print("colored : ", colored)
-#print("color : ", color)
-#print("graph : ", graph)
-"""
-        if(colored[v1-1]==0):
-            colored[v1-1] = 1
-            graph[v1-1] = graph[v2-1] ^ 1
-
-        if(colored[v2-1]==0):
-            colored[v2-1] = 1
-            graph[v2-1] = graph[v1-1]^ 1
-
-        if(graph[v2-1] == graph[v1-1]):
-            answer = False
-            break
-        """
 

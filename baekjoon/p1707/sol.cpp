cdd034521ddc8568e25282c0d0c3672a017af547
kimsanghyun <garzets@gmail.com>
2021-09-19 20:56:05 +0900

    p1707

---
+++ baekjoon/p1707/sol.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int first, second;
+
+    cout << "test\n";
+    cin >> first >> second ;
+    cout << "first" << first;
+    cout << "second" << second;
+    
+    return 0;
+}
